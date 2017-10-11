c48eb15a0c369f972529c374262618f3d2bf6b82
Tiloaria <savonula@yandex.ru>
2017-10-11 18:40:20 +0300

    For future

---
+++ HashMap/src/main/java/HashMap.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.Set;
+
+//Не поняла задания, совсем
+public interface HashMap extends Map {
+    int current = 1;
+    Set<> iterators;
+    @Override
+    Set<Entry> entrySet();
+    public Set<Entry<>> iterator();
+
+    @Override
+    int size();
+}
