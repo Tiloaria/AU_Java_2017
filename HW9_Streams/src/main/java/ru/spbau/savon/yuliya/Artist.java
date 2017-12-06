36dc25a914d9ea6631b1d49afa5e4ddce67b0e07
Tiloaria <savonula@yandex.ru>
2017-12-06 15:22:46 +0300

    Streams

---
+++ HW9_Streams/src/main/java/ru/spbau/savon/yuliya/Artist.java
@@ -0,0 +1,14 @@
+package ru.spbau.savon.yuliya;
+
+public class Artist {
+
+    private final String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
