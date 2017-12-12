7e15b4a98f5e2b5644f38d3bdd306888a33ef859
Tiloaria <savonula@yandex.ru>
2017-12-13 02:38:56 +0300

    added tests, corrected stylegyde mistakes

--- HW2_Spriral/src/main/java/Spiral.java
+++ HW2_Spriral/src/main/java/Spiral.java
@@ -6,18 +6,23 @@ import java.util.Comparator;
  * @author Savon Yuliya
  */
 
+/**
+ * Exist matrix N*N with int values ans odd N.
+ * getSpriral -  out elements of matrix.
+ * sort - sort columns of matrix by first elements.
+ */
 public class Spiral {
     private int size = 1;
     private int[][] matrix = new int[1][1];
 
     /**
      * check new_matrix size and if it's ok create new matrix
-     * @param new_size
-     * @param new_matrix
+     *
+     * @param new_size new size of matrix
+     * @param new_matrix new matrix
      */
-
-    public Spiral(int new_size, int [] [] new_matrix) {
-        if((new_size % 2 == 1) & (new_matrix.length == new_size) & (new_matrix[0].length == new_size)) {
+    public Spiral(int new_size, int[][] new_matrix) {
+        if ((new_size % 2 == 1) & (new_matrix.length == new_size) & (new_matrix[0].length == new_size)) {
             matrix = new_matrix;
             size = new_size;
         }
@@ -25,24 +30,24 @@ public class Spiral {
 
     /**
      * return ArrayList with numbers from array by spiral
-     * @return
+     *
+     * @return return ArrayList with values bypass spiral
      */
-
     public ArrayList<Integer> getSpiral() {
 
         int center = size / 2;
         int currentX = center;
         int currentY = center;
-        int [] dX = new int[] {1, 0, -1, 0};
-        int [] dY = new int[] {0, 1, 0, -1};
+        int[] dX = new int[]{1, 0, -1, 0};
+        int[] dY = new int[]{0, 1, 0, -1};
         int len = 2;
         ArrayList<Integer> out = new ArrayList<>();
         out.add(matrix[currentX][currentY]);
-        while(len < size) {
+        while (len < size) {
             currentX--;
             currentY--;
-            for(int i = 0; i < 4; i++) {
-                for(int j = 0; j < len; j++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < len; j++) {
                     currentX += dX[i];
                     currentY += dY[i];
                     out.add(matrix[currentX][currentY]);
@@ -57,14 +62,14 @@ public class Spiral {
      * sort matrix strings by first element
      */
     public void sort() {
-        Arrays.sort(matrix, new Comparator<int []>() {
-            public int compare(int [] o1, int[] o2) {
+        Arrays.sort(matrix, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
                 return Integer.compare(o1[0], o2[0]);
             }
         });
     }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int[][] a = new int[][]{{9, 8, 7}, {2, 1, 6}, {3, 4, 5}};
         Spiral spiral = new Spiral(3, a);
         System.out.println(spiral.getSpiral());
--- HW2_Spriral/src/test/java/SpiralTest.java
+++ HW2_Spriral/src/test/java/SpiralTest.java
@@ -6,20 +6,33 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class SpiralTest {
+    Spiral spiralOwn;
     Spiral spiral;
+    Spiral spiralFive;
 
     @Before
-    public void  SetUp(){
-        int[][] b = new int[][]{{9, 8, 7}, {2, 1, 6}, {3, 4, 5}};
-        Spiral spiral = new Spiral(3, b);
+    public void setUp() {
+        int[][] own = new int[][]{{1}};
+        spiralOwn = new Spiral(1, own);
+
+        int[][] a = new int[][]{{9, 8, 7}, {2, 1, 6}, {3, 4, 5}};
+        spiral = new Spiral(3, a);
+
+        int[][] five = new int[][]{{5, 5, 5}, {3, 3, 3}, {1, 1, 1}};
+        spiralFive = new Spiral(3, five);
     }
 
     /**
      * check correct of output Array
+     *
      * @throws Exception
      */
     @Test
     public void getSpiral() throws Exception {
+        ArrayList<Integer> ownA = new ArrayList<>();
+        ownA.add(1);
+        assertEquals(ownA, spiralOwn.getSpiral());
+
         ArrayList<Integer> a = new ArrayList<>();
         a.add(1);
         a.add(2);
@@ -33,14 +46,32 @@ public class SpiralTest {
         a.add(8);
         a.add(9);
         assertEquals(a, spiral.getSpiral());
+
+        ArrayList<Integer> five = new ArrayList<>();
+        five.add(3);
+        five.add(3);
+        five.add(1);
+        five.add(1);
+        five.add(1);
+        five.add(3);
+        five.add(5);
+        five.add(5);
+        five.add(5);
+        assertEquals(five, spiralFive.getSpiral());
     }
 
     /**
      * check correct of spiral array after sort
+     *
      * @throws Exception
      */
     @Test
     public void sort() throws Exception {
+        ArrayList<Integer> ownA = new ArrayList<>();
+        ownA.add(1);
+        spiralOwn.sort();
+        assertEquals(ownA, spiralOwn.getSpiral());
+
         ArrayList<Integer> a = new ArrayList<>();
         a.add(4);
         a.add(3);
@@ -55,6 +86,20 @@ public class SpiralTest {
         a.add(2);
         spiral.sort();
         assertEquals(a, spiral.getSpiral());
+
+        ArrayList<Integer> five = new ArrayList<>();
+        five.add(3);
+        five.add(3);
+        five.add(5);
+        five.add(5);
+        five.add(5);
+        five.add(3);
+        five.add(1);
+        five.add(1);
+        five.add(1);
+
+        spiralFive.sort();
+        assertEquals(five, spiralFive.getSpiral());
     }
 
 }
