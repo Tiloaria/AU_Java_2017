ed442ec070262a128842e3c380304abf3fe5e04e
Tiloaria <savonula@yandex.ru>
2017-12-20 18:19:25 +0300

    dfas

---
+++ HW4_Trie/src/test/java/ru/spbau/savon/hw3/TrieTest.java
@@ -0,0 +1,28 @@
+package ru.spbau.savon.hw3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrieTest {
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void contains() throws Exception {
+    }
+
+    @Test
+    public void remove() throws Exception {
+    }
+
+    @Test
+    public void size() throws Exception {
+    }
+
+    @Test
+    public void howManyStartWithPrefix() throws Exception {
+    }
+
+}
