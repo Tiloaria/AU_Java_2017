e52842fe8c0762c6ce401463ee660c758ed1a383
Tiloaria <savonula@yandex.ru>
2017-12-22 19:53:13 +0300

    all functions in FirstPart works

--- HW9_Streams/src/main/java/ru/spbau/savon/yuliya/FirstPartTasks.java
+++ HW9_Streams/src/main/java/ru/spbau/savon/yuliya/FirstPartTasks.java
@@ -1,13 +1,19 @@
 package ru.spbau.savon.yuliya;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public final class FirstPartTasks {
 
-    private FirstPartTasks() {}
+    private FirstPartTasks() {
+    }
 
     // Список названий альбомов
     public static List<String> allNames(Stream<Album> albums) {
@@ -21,12 +27,17 @@ public final class FirstPartTasks {
 
     // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
     public static List<String> allTracksSorted(Stream<Album> albums) {
-        return albums.flatMap(x -> x.getTracks().stream()).map(Track::getName).sorted().collect(Collectors.toList());
+        return albums.flatMap(x -> x.getTracks().stream())
+                .map(Track::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
     public static List<Album> sortedFavorites(Stream<Album> s) {
-        return s.filter(x -> goodRating(x)).sorted(Comparator.comparing(Album::getName)).collect(Collectors.toList());
+        return s.filter(a -> a.getTracks().stream().anyMatch(track -> track.getRating() > 95))
+                .sorted(Comparator.comparing(Album::getName))
+                .collect(Collectors.toList());
     }
 
     private static boolean goodRating(Album album) {
@@ -46,23 +57,49 @@ public final class FirstPartTasks {
 
     // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
     public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
-        throw new UnsupportedOperationException(); //
+        return albums.collect(Collectors.groupingBy(
+                Album::getArtist, Collectors.mapping(
+                        Album::getName, Collectors.toList()
+                )
+        ));
     }
 
     // Число повторяющихся альбомов в потоке
     public static long countAlbumDuplicates(Stream<Album> albums) {
-        throw new UnsupportedOperationException(); //
+
+        return albums.sorted(Comparator
+                .comparing(Album::getName)
+                .thenComparing(a -> a.getArtist().getName()))
+                .filter(new Predicate<Album>() {
+                    Album previousAlbum = null;
+
+                    @Override
+                    public boolean test(Album album) {
+                        boolean ret = album.equals(previousAlbum);
+                        previousAlbum = album;
+                        return ret;
+                    }
+                }).distinct().count(); //
     }
 
     // Альбом, в котором максимум рейтинга минимален
     // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
     public static Optional<Album> minMaxRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException(); //
+        return albums.min(Comparator.comparing(album -> album.getTracks()
+                        .stream().mapToInt(Track::getRating).max().orElse(0)));
     }
 
     // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
     public static List<Album> sortByAverageRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();//
+        return albums.sorted(Comparator
+                        .comparingDouble((ToDoubleFunction<Album>) album -> album
+                                .getTracks()
+                                .stream()
+                                .mapToInt(Track::getRating)
+                                .average()
+                                .orElse(0))
+                        .reversed())
+                .collect(Collectors.toList());//
     }
 
     // Произведение всех чисел потока по модулю 'modulo'
@@ -74,7 +111,8 @@ public final class FirstPartTasks {
     // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
     // см. тесты
     public static String joinTo(String... strings) {
-        return Arrays.stream(strings).collect(Collectors.joining(", ", "<", ">"));  }
+        return Arrays.stream(strings).collect(Collectors.joining(", ", "<", ">"));
+    }
 
     // Вернуть поток из объектов класса 'clazz'
     public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
