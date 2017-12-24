8acf543e4280e62845245c3f399c64210a446e0d
Tiloaria <savonula@yandex.ru>
2017-12-24 14:35:59 +0300

    added serialize, deserialize and tests

--- HW4_Trie/src/main/java/ru/spbau/savon/hw3/Trie.java
+++ HW4_Trie/src/main/java/ru/spbau/savon/hw3/Trie.java
@@ -1,20 +1,22 @@
 package ru.spbau.savon.hw3;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 
 /**
  * Class can save string, add and remove strings in structure.
  * Also with fast time return size of Trie, is the string contains this array,
  * number of strings with prefix.
- *
  */
-public class Trie {
+public class Trie implements Serializable {
 
-    private int size; //number of strings
-    private Point start; //the root of Trie
+    private int size = 0; //number of strings
+    private Point start = new Point(); //the root of Trie
 
     /**
-     * add string to Trie and make new Points if it's necessary
+     * add string to Trie and make new Points if it's necessary, works for O(|length|)
      * @param element string which we want to add
      * @return true if we add new string, false otherwise
      */
@@ -23,11 +25,13 @@ public class Trie {
         for (int i = 0; i < element.length(); i++) {
             current = current.goTo(element.charAt(i));
         }
+
         if (current.setEndOfString()) {         //check if it was the same sting
             current = start;
+            current.increaseStartsWithPrefix();
             for (int i = 0; i < element.length(); i++) {  //update startsWithPrefix
+                current = current.getNext(element.charAt(i));
                 current.increaseStartsWithPrefix();
-                current.getNext(element.charAt(i));
             }
             size++;
             return true;
@@ -37,22 +41,22 @@ public class Trie {
 
     /**
      * check contains sting in Trie
-     * @param element is checked string
+     * @param element checked string
      * @return true if string contains, false otherwise
      */
     public boolean contains(String element) {
         Point current = start;
         for (int i = 0; i < element.length(); i++) {
             current = current.getNext(element.charAt(i));
-            if (current.getNext(element.charAt(i)) == null) {
+            if (current == null) {
                 return false;
             }
         }
-        return current.getEndOfString();
+        return current.isEndOfString();
     }
 
     /**
-     * remove sting from Trie
+     * remove string from Trie, works for O(|length|)
      * @param element removing string
      * @return true if string contained in Trie, false otherwise
      */
@@ -61,14 +65,16 @@ public class Trie {
         Point current = start;
         for (int i = 0; i < element.length(); i++) {
             current = current.getNext(element.charAt(i));
-            if (current.getNext(element.charAt(i)) == null) {
+            if (current == null) {
                 return false;
             }
         }
         if (current.removeEndOfString()) {
+            current = start;
+            current.reduceStartsWithPrefix();
             for (int i = 0; i < element.length(); i++) {
+                current = current.getNext(element.charAt(i));
                 current.reduceStartsWithPrefix();
-                current.getNext(element.charAt(i));
             }
             size--;
             return true;
@@ -79,19 +85,19 @@ public class Trie {
     /**
      * @return number of different string in Trie
      */
-    public  int size() {
+    public int size() {
         return size;
     }
 
     /**
-     * count during O(|prefix|) number of strings in Trie with this prefix
+     * count during O(|prefix|) number of strings in Trie with this prefix, works for O(|prefix|)
      * @param prefix
      * @return number of sting, if prefix isn't contained in Trie return 0
      */
-    int howManyStartWithPrefix(String prefix) {
+    public int howManyStartWithPrefix(String prefix) {
         Point current = start;
         for (int i = 0; i < prefix.length(); i++) {
-            current.getNext(prefix.charAt(i));
+            current = current.getNext(prefix.charAt(i));
             if (current == null) {
                 return 0;
             }
@@ -102,13 +108,17 @@ public class Trie {
     /**
      * helper class have private fields next, endOfString, startsWithPrefix
      */
-    private class Point {
-        private int countOfLetters = 65536; //max Char
-        private Point [ ] next = new Point[countOfLetters]; //link to all potential branches
+    private class Point implements Serializable {
+        private int countOfLetters = 255; //max Char
+        private Point[] next = new Point[countOfLetters]; //link to all potential branches
         private boolean endOfString = false;
         private int startsWithPrefix = 0;
 
-        public boolean getEndOfString() {
+        public int getCountOfLetters() {
+            return countOfLetters;
+        }
+
+        public boolean isEndOfString() {
             return endOfString;
         }
 
@@ -133,7 +143,7 @@ public class Trie {
         }
 
         /**
-         * try to go to the next Point, if it's null create new Point
+         * try to go to the next Point, if it's null create new Point, O(1)
          * @param letter current char
          * @return next Point
          */
@@ -155,6 +165,61 @@ public class Trie {
 
         public void reduceStartsWithPrefix() {
             startsWithPrefix--;
+        }
+    }
+
+    /**
+     * bypasses Trie, and write exist strings in OutputStream, after every string write 0
+     * @param buffer save the current string
+     * @param v current Point in Trie
+     * @param out output stream to write strings
+     * @throws IOException uncorrected write
+     */
+    private void dfs(StringBuffer buffer, Point v, OutputStream out) throws IOException {
+        if (v == null)
+            return;
+        if (v.isEndOfString()) {
+            out.write(buffer.toString().getBytes());
+            out.write(0);
+        }
+        for (int i = 0; i < v.getCountOfLetters(); i++) {
+            dfs(buffer.append((char) i), v.next[i], out);
+            buffer.delete(buffer.length() - 1, buffer.length());
+        }
+    }
+
+    /**
+     * serialize Trie to Stream. Decide that it's 255 or less strings in Trie.
+     * @param out OutputStream where trie be written
+     * @throws IOException - uncorrected OutputStream
+     */
+    public void serialize(OutputStream out) throws IOException {
+        out.write((char) size);
+        dfs(new StringBuffer(), start, out);
+        out.write(0);
+    }
+
+    /**
+     * serialize Trie from Stream
+     * @param in input stream from trie will be read
+     * @throws IOException uncorrected InputStream
+     */
+    public void deserialize(InputStream in) throws IOException {
+        size = 0;
+        int mustSize = in.read();
+        start = new Point();
+        StringBuffer buf = new StringBuffer();
+        while (in.available() > 1) {
+            int first = in.read();
+            if (first == 0) {
+                add(buf.toString());
+                buf.delete(0, buf.length());
+            } else {
+                buf.append((char) first);
+            }
+        }
+        if (size != mustSize) {
+            throw new IOException();
         }
     }
 }
